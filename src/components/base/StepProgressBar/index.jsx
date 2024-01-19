616fd4df4ed0fcc5b42c7b988fe8015caabd90ee
Alessandro Manfredi <alle.manfredi@gmail.com>
2024-01-19 07:29:06 +0100

    refactor(components): changes StepProgressBar filledBackground

--- src/components/base/StepProgressBar/index.jsx
+++ src/components/base/StepProgressBar/index.jsx
@@ -3,7 +3,7 @@ import { ProgressBar, Step } from 'react-step-progress-bar'
 
 const StepProgressBar = (_props) => {
   return (
-    <ProgressBar {..._props} filledBackground="linear-gradient(to right, #DFF9E6, #23C55E)">
+    <ProgressBar {..._props} filledBackground="linear-gradient(to right, #E3D1FF, #B593FF)">
       {_props.stepPositions.map((_, _index) => (
         <Step transition="scale" key={`step_progress_bar_${_index}`}>
           {({ accomplished }) => (
